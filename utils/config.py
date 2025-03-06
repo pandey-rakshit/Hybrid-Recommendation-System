9b409e3ea0c1ba9412ccb4fcd1cff23a33cc2da6
pandey-rakshit <inbox.pandeyrakshit@gmail.com>
2025-03-06 20:17:17 +0530

    ADD: Data Loading and cleaning

---
+++ utils/config.py
@@ -0,0 +1,9 @@
+import os
+
+# Root directory of the project
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+# Data paths
+DATA_DIR = os.path.join(PROJECT_ROOT, "data")
+MOVIES_PATH = os.path.join(DATA_DIR, "movies.csv")
+ANIME_PATH = os.path.join(DATA_DIR, "anime.csv")
